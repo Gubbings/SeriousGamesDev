430e69e5998595e74a9f006544663f67771001ee
Guy C <guy.coccimiglio@gmail.com>
2017-11-26 03:41:14 -0500

    Updated Tutorial
    Added further dialog to include chord progression

--- Assets/Scripts/TutorialManager.cs
+++ Assets/Scripts/TutorialManager.cs
@@ -6,9 +6,21 @@ using UnityEngine.UI;
 public class TutorialManager : MonoBehaviour {
 
 
-    public Text textObject;
+    public Text textObject1;
+    public Text textObject2;
+
+    public GameObject Tut1_Panel;
+    public GameObject Tut2_Panel;
+    public GameObject staff;
+
+    public GameObject signatureButton;
+    public GameObject signaturePanel;
+
+    public GameObject chordTypeButton;
+    public GameObject chordTypePanel;
 
     private int tutorialStage = 0;
+    
 
 	// Use this for initialization
 	void Start () {
@@ -26,24 +38,90 @@ public class TutorialManager : MonoBehaviour {
 
         switch (tutorialStage) {
             case 1:
-                textObject.text = "Classical harmony has many aspects but we will focus on cord progressions. " +
-                    "We can label cords of a key numerical starting from the base note of the key. For example in C major cord 1 is C and cord 2 is D.";
+                textObject1.text = "Classical harmony has many aspects but we will focus on cord progressions. " +
+                    "We can label chords of a key numerically starting from the base note of the key. For example in C major chord 1 is C and chord 2 is D.";
                 break;
+
             case 2:
-                textObject.text = "This means you are going to need to know your key signatures!\n" +
-                    "We will focus on regular major keys. " +
-                    "Lets take a look at the key signatures for the regular note major keys.";
+                textObject1.text = "This means you are going to need to know your key signatures!\n" +
+                    "We will focus on simple major keys.";                  
                 break;
-            case 3:
-                textObject.text = "C Major - no sharps\n" +
+
+            case 3:            
+                Tut1_Panel.SetActive(false);
+                staff.SetActive(true);
+                Tut2_Panel.SetActive(true);
+
+                textObject2.text = "The key signature will be shown on the left of the staff next to the treble cleff. We will use examples with at most 5 sharps shown here.\n" +
+                    "Lets take a look at the key signatures for the keys that we will focus on.";
+                break;
+
+            case 4:
+                Tut1_Panel.SetActive(false);
+                staff.SetActive(true);
+                Tut2_Panel.SetActive(true);
+
+                textObject2.text = "C Major - no sharps\n" +
                     "D Major - F# C#\n " +
                     "E Major - F# C# G# D#\n " +
-                    "F Major - A#\n" +
+                    "F Major - B♭\n" +                    
                     "G Major - F#\n" +
                     "A Major - F# C# G#\n" +
                     "B Major-  F# C# G# D# A#";
                 break;
+
+            case 5:
+                signatureButton.SetActive(true);
+
+                textObject2.text = "Well keep those tucked away to the left. You can click the arrow on the left to open the key signature list.";
+                break;
+
+            case 6:  
+                textObject2.text = "Now onto the real problem. Chord progression. A harmonic progression consists of the following sequence of chord classifications:\n" +
+                    "Tonic - Predominant - Dominant - Tonic.";
+                break;
+
+            case 7:
+                textObject2.text = "Each cord classification represents a set of chords based on the key.\n" +
+                    "Tonic has chords 1 and 6.\n" +
+                    "Predominant has chords 2, 3, 4 and 6.\n" +
+                    "Dominant has chords 5 and 7.";
+                break;
+
+            case 8:
+                chordTypeButton.SetActive(true);
+
+                textObject2.text = "Anyone can read the notes but you will need to identify it by ear. To help you out we will keep the chord classification available on the right.\n" +
+                    "You can click the arrow on the right to open the chord classification list.";
+                break;
+
+            case 9:                
+                textObject2.text = "Now that you have everything you need I will give you a sequence of 8 notes. Like this.";
+                break;
+
+            case 10:
+                textObject2.text = "But some of the notes will be missing. You need to listen carefully to decide what those missing notes are. Then play them on this piano.";
+                break;
         }
     }
 
+
+    public void ToggleSignaturePanel() {
+        if (signaturePanel.activeSelf) {
+            signaturePanel.SetActive(false);
+        }
+        else {
+            signaturePanel.SetActive(true);
+        }
+    }
+
+
+    public void ToggleChordTypePanel() {
+        if (chordTypePanel.activeSelf) {
+            chordTypePanel.SetActive(false);
+        }
+        else {
+            chordTypePanel.SetActive(true);
+        }
+    }
 }
